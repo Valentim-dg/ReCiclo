62c10729940fb1896da2fbfb181ab1ae5c2181bf
Valentim Dutra <dutragarciav@gmail.com>
2025-07-21 15:16:59 -0300

    feat: adiciona gestão de conquistas e usuários no painel de administração

--- controller/api/admin.py
+++ controller/api/admin.py
@@ -1,31 +1,79 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Achievement, UserAchievement
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
-# Register your models here.
-
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    # Campos a serem exibidos na lista de usuários no Django Admin
-    list_display = ['username', 'email', 'recycling_coins', 'achievements',
-                    'reputation_coins', 'level', 'is_active', 'date_joined']
+    """
+    Configuração personalizada para a exibição e gestão do modelo CustomUser
+    no painel de administração do Django.
+    """
+    # Define os formulários customizados para a criação e edição de utilizadores.
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
 
-    # Configuração dos campos no formulário de detalhes do usuário
+    model = CustomUser
+
+    # Define as colunas a serem exibidas na lista de utilizadores.
+    list_display = [
+        'username',
+        'email',
+        'is_curator',
+        'is_staff',
+        'level',
+        'recycling_coins',
+        'reputation_coins',
+        'unlocked_achievements_count'
+    ]
+
+    # Organiza os campos no formulário de edição de um utilizador existente.
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('recycling_coins', 'reputation_coins',
-         'level', 'achievements', 'recycling_history')}),
+        ('Campos Customizados', {'fields': (
+            'recycling_coins',
+            'reputation_coins',
+            'level',
+            'experience',
+            'profile_image',
+            'is_curator'
+        )}),
     )
 
-    # Campos que podem ser editados diretamente na lista de usuários
-    list_filter = UserAdmin.list_filter + \
-        ('recycling_coins', 'reputation_coins', 'level')
-
-    # Configurações dos campos que aparecem no formulário de edição do usuário
+    # Organiza os campos no formulário de criação de um novo utilizador.
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('recycling_coins', 'reputation_coins',
-         'level', 'achievements', 'recycling_history')}),
+        (None, {'fields': ('is_curator',)}),
     )
 
+    def unlocked_achievements_count(self, obj):
+        """
+        Calcula e retorna a contagem de conquistas desbloqueadas para um utilizador.
+        Este método é usado para criar uma coluna calculada em 'list_display'.
+        """
+        return obj.user_achievements.filter(unlocked_at__isnull=False).count()
 
-# Registrando o modelo CustomUser com a configuração personalizada do Admin
-admin.site.register(CustomUser, CustomUserAdmin)
+    # Define um nome amigável para a coluna no painel de administração.
+    unlocked_achievements_count.short_description = 'Conquistas Desbloqueadas'
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    """
+    Configuração para a gestão do modelo Achievement no painel de administração.
+    Permite criar e editar as definições de todas as conquistas da plataforma.
+    """
+    list_display = ('id', 'title', 'description',
+                    'criteria_type', 'criteria_value')
+    list_filter = ('criteria_type',)
+    search_fields = ('id', 'title', 'description')
+
+
+@admin.register(UserAchievement)
+class UserAchievementAdmin(admin.ModelAdmin):
+    """
+    Configuração para a gestão do modelo UserAchievement no painel de administração.
+    Permite visualizar e gerir as relações entre utilizadores e as suas conquistas.
+    """
+    list_display = ('user', 'achievement', 'unlocked_at')
+    list_filter = ('achievement', 'unlocked_at')
+    search_fields = ('user__username', 'achievement__title')
